759b1c4d449cdccb532344ae20315268f3b669b5
Frank Zheng <frankzheng@me.com>
2020-04-10 00:14:51 -0500

    remove website links from TOC

--- static/js/fix-toc.js
+++ static/js/fix-toc.js
@@ -8,3 +8,28 @@
   // remove <ul><li></li></ul> where <ul> only contains one <li>
   ul.outerHTML = li.innerHTML;
 })();
+
+function remove_toc_links() {
+  var toc = document.getElementById('TableOfContents');
+  if (!toc) return;
+
+  lis = toc.getElementsByTagName('li')
+  for (li of lis) {
+
+    str = li.innerHTML
+    if (str.includes('-https-') && !str.includes('<ul>')) {
+
+      begin = str.indexOf('"><')
+      end = str.search('target="_blank') + 14
+
+      delete_text = str.substring(begin, end)
+
+      // add tags and remove the website hyperlinks from TOC
+      str_real = '<li>' + str.replace(delete_text, '') + '</li>'
+      // console.log(str_real)
+      li.outerHTML = str_real
+    }
+  }
+}
+
+remove_toc_links()
